b6f8fdf4f935e168a2df6dcd88f9f6b84b46ba7e
kachito <lenin.yepez@gmail.com>
2017-12-29 06:50:42 -0500

    hola
    subir

---
+++ login/SesionCollector.php
@@ -0,0 +1,22 @@
+<?php
+include_once('Sesion.php');
+include_once('Collector.php');
+class SesionCollector extends Collector
+{
+  function showSesion()
+  {
+    $rows = self::$db->getRows("SELECT *FROM sesion ");
+    $arraySesion = array();
+    foreach ($rows as $c) {
+      $aux = new Sesion($c{'id_sesion'}, $c{'usuario'}, $c{'pasw'});
+      array_push($arraySesion, $aux);
+    }
+    return $arraySesion;
+  }
+  function insertSesion($id_sesion, $usuario, $pasw)
+  {
+    $new_row = self::$db->getRow("Insert into sesion (id_sesion, usuario, pasw) values ('$id_sesion','$usuario','$pasw')");
+    return 1;
+  }
+}
+?>
