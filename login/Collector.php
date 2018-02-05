f6cd390a4b636bb70841eecd5b13b1d2a595bc5d
kachito <lenin.yepez@gmail.com>
2018-02-05 06:36:53 -0500

    Update Collector.php

--- login/Collector.php
+++ login/Collector.php
@@ -2,10 +2,10 @@
 
 include_once('dataBase.php');
 // Define configuration
-define("DB_HOST", "localhost");
-define("DB_USER", "postgres");
-define("DB_PASS", "12345");
-define("DB_NAME", "edusmart");
+define("DB_HOST", "ec2-107-22-174-187.compute-1.amazonaws.com");
+define("DB_USER", "osqssxvutetuye");
+define("DB_PASS", "5e7c7f6aa9e3d5648e0aa2cf701be17cd112ebe8df7bffed80a9c0ab22585c8b");
+define("DB_NAME", "d7ldr81s6865d4");
 class Collector extends dataBase
 {
   public static $db;
